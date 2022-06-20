82b34b33769515810ae5974b5126a13e87821037
Dataa013 <gvaradzedata@gmail.com>
2022-06-21 02:58:09 +0400

    initialcommit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const navBtn = document.querySelector('.nav-btn');
+// const btn = document.querySelector('.nav-btn-button')
+
+navBtn.addEventListener('click', function() {
+    navBtn.classList.toggle('show-sign');
+})
